60d15cd2e7c8bf8f5783826389fb170a183b8331
Mahmut Yazar <mahmutyazarios@gmail.com>
2023-01-15 23:57:22 +0300

    Project created.
    Project files have been uploaded to Github.

---
+++ RAWG GAMES/RAWG GAMES/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RAWG GAMES
+//
+//  Created by Mahmut Yazar on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
