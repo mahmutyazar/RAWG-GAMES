e69abbd610379ce83f15185a28c2f4d8f62a745a
Mahmut Yazar <mahmutyazarios@gmail.com>
2023-01-16 10:09:58 +0300

    Infra/Dependencies added
    Alamofire and Kingfisher added by using SPM.

---
+++ RAWG GAMES/RAWG GAMES/Modules/Home Screen/HomeModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeModel.swift
+//  RAWG GAMES
+//
+//  Created by Mahmut Yazar on 16.01.2023.
+//
+
+import Foundation
+import Alamofire
+
+class HomeModel {
+    
+    let apiKey: String = "ed862e3ef473469890abd5142066f509"
+    
+    
+    func fetcData() {
+        
+    //https://api.rawg.io/api/games
+        
+    }
+    
+}
---
+++ RAWG GAMES/RAWG GAMES/Modules/Home Screen/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  RAWG GAMES
+//
+//  Created by Mahmut Yazar on 16.01.2023.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    
+    
+}
