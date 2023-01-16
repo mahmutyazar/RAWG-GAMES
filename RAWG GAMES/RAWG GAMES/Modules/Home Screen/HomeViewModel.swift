febf36a34c553944888e63bda90431e3b4e26293
Mahmut Yazar <mahmutyazarios@gmail.com>
2023-01-16 18:11:57 +0300

    TableView designed.
    Fundamental elements of home page created.

--- RAWG GAMES/RAWG GAMES/Modules/Home Screen/HomeViewModel.swift
+++ RAWG GAMES/RAWG GAMES/Modules/Home Screen/HomeViewModel.swift
@@ -12,6 +12,7 @@ class HomeViewModel {
     private let model = HomeModel()
     
     var errorCaught: ((String) -> ())?
+    var loadItems: (([HomeCellModel]) -> ())?
     
     init() {
         model.delegate = self
@@ -22,13 +23,15 @@ class HomeViewModel {
     }
     
     func itemPressed(_ index: Int) {
-        //TODO:
+        //TODO:  
     }
 }
 
 extension HomeViewModel: HomeModelProtocol {
     func didDataFetch() {
-        <#code#>
+        let homeCellModel: [HomeCellModel] = model.data.map { .init(id: $0.id ?? 0, name: $0.name ?? "", backgroundImage: $0.backgroundImage ?? "", released: $0.released ?? "", rating: $0.rating ?? 0.0, ratingTop: $0.ratingTop ?? 0, genre: $0.genres! )}
+        
+        loadItems?(homeCellModel)
     }
     
     func didDataCouldntFetch() {
