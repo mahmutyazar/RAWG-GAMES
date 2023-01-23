11b6dd5b7a779bd88a2c62485d2bc40ee3f2cd5c
Mahmut Yazar <mahmutyazarios@gmail.com>
2023-01-23 19:02:48 +0300

    MainPage Sort and Local Push Notification completed.

--- RAWG GAMES/RAWG GAMES/AppDelegate.swift
+++ RAWG GAMES/RAWG GAMES/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { granted, error in }
+        
         return true
     }
 
